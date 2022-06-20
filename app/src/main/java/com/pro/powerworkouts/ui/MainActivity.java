2485dbf1a5f624cd8de3d5246cc9171ff1e8ff27
Okellothomas <thomasolal33@gmail.com>
2022-06-20 21:24:54 +0300

    connect database to the project

--- app/src/main/java/com/pro/powerworkouts/ui/MainActivity.java
+++ app/src/main/java/com/pro/powerworkouts/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.pro.powerworkouts;
+package com.pro.powerworkouts.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.pro.powerworkouts.R;
 
 public class MainActivity extends AppCompatActivity {
 
