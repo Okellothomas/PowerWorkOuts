a729b689745f2ac7c998802602000b9af7f3b98e
Okellothomas <thomasolal33@gmail.com>
2022-06-21 07:39:29 +0300

    add the while button drawable file

---
+++ app/src/main/java/com/pro/powerworkouts/ui/SignUp.java
@@ -0,0 +1,16 @@
+package com.pro.powerworkouts.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pro.powerworkouts.R;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
