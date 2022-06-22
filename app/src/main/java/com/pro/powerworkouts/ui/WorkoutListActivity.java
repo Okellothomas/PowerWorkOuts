f1db940be5a8a3281beca0de448908f29800356a
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-23 02:54:35 +0300

    Update logic

--- app/src/main/java/com/pro/powerworkouts/ui/WorkoutListActivity.java
+++ app/src/main/java/com/pro/powerworkouts/ui/WorkoutListActivity.java
@@ -6,20 +6,22 @@ import static com.pro.powerworkouts.util.UIHelpers.displayData;
 import static com.pro.powerworkouts.util.UIHelpers.displayError;
 import static com.pro.powerworkouts.util.UIHelpers.hideProgressDialog;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
-
+import com.bumptech.glide.Glide;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.pro.powerworkouts.R;
 import com.pro.powerworkouts.adapter.WorkoutListAdapter;
 import com.pro.powerworkouts.interfaces.OnClickListener;
@@ -27,8 +29,6 @@ import com.pro.powerworkouts.models.Workout;
 import com.pro.powerworkouts.util.Constants;
 
 import java.util.List;
-import java.util.Locale;
-import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -45,6 +45,10 @@ public class WorkoutListActivity extends AppCompatActivity implements OnClickLis
   TextView progressText;
   @BindView(R.id.error_text)
   TextView errorText;
+  @BindView(R.id.toolbar)
+  CollapsingToolbarLayout toolbar;
+  @BindView(R.id.workout_category_image)
+  ImageView categoryImage;
 
   public static final String TAG = WorkoutListActivity.class.getSimpleName();
 
@@ -55,8 +59,9 @@ public class WorkoutListActivity extends AppCompatActivity implements OnClickLis
     ButterKnife.bind(this);
 
     String workoutCategory = getIntent().getStringExtra(Constants.EXTRA_WORKOUT_CATEGORY);
-    Objects.requireNonNull(getSupportActionBar()).setTitle(String.format("%s workouts", capitalize(workoutCategory)));
+    toolbar.setTitle(String.format("%s workouts", capitalize(workoutCategory)));
 
+    setCategoryImage(workoutCategory);
     loadWorkouts(workoutCategory);
   }
 
@@ -97,6 +102,44 @@ public class WorkoutListActivity extends AppCompatActivity implements OnClickLis
       workoutList.setLayoutManager(new GridLayoutManager(getApplicationContext(), getResources().getInteger(R.integer.grid_columns_2)));
     } else {
       workoutList.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+    }
+  }
+
+  private void setCategoryImage(String category){
+    switch (category){
+      case "back":
+        Glide.with(this).asBitmap().load(R.drawable.back_workout).into(categoryImage);
+        break;
+      case "cardio":
+        Glide.with(this).asBitmap().load(R.drawable.cardio_workout).into(categoryImage);
+        break;
+      case "chest":
+        Glide.with(this).asBitmap().load(R.drawable.chest_workout).into(categoryImage);
+        break;
+      case "lower arms":
+        Glide.with(this).asBitmap().load(R.drawable.lower_arms_workout).into(categoryImage);
+        break;
+      case "lower legs":
+        Glide.with(this).asBitmap().load(R.drawable.lower_legs_workout).into(categoryImage);
+        break;
+      case "neck":
+        Glide.with(this).asBitmap().load(R.drawable.neck_workout).into(categoryImage);
+        break;
+      case "shoulders":
+        Glide.with(this).asBitmap().load(R.drawable.shoulder_workout).into(categoryImage);
+        break;
+      case "upper arms":
+        Glide.with(this).asBitmap().load(R.drawable.upper_arms_workout).into(categoryImage);
+        break;
+      case "upper legs":
+        Glide.with(this).asBitmap().load(R.drawable.upper_legs_workout).into(categoryImage);
+        break;
+      case "waist":
+        Glide.with(this).asBitmap().load(R.drawable.waist_workout).into(categoryImage);
+        break;
+      default:
+        Glide.with(this).asBitmap().load(R.drawable.ic_baseline_fitness_center).into(categoryImage);
+        break;
     }
   }
 
