2fd6816bcd0f666fb3706179d7af0505a54940a4
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-22 11:24:39 +0300

    Add constants

---
+++ app/src/main/java/com/pro/powerworkouts/util/Constants.java
@@ -0,0 +1,7 @@
+package com.pro.powerworkouts.util;
+
+public class Constants {
+  public static final String BASE_URL = "https://exercisedb.p.rapidapi.com/exercises/";
+  public static final String HOST_QUERY_PARAMETER = "X-RapidAPI-Host";
+  public static final String KEY_QUERY_PARAMETER = "X-RapidAPI-Key";
+}
