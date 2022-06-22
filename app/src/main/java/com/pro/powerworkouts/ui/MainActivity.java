e2aae55557022dd854c94cc1fdc08da2890fce24
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-22 19:00:54 +0300

    Update logic

--- app/src/main/java/com/pro/powerworkouts/ui/MainActivity.java
+++ app/src/main/java/com/pro/powerworkouts/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.pro.powerworkouts.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.pro.powerworkouts.R;
@@ -12,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, HomeActivity.class));
+        finish();
     }
 }
