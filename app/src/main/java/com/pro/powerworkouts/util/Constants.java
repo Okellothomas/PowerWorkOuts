4f218d9764c058fa89ba86c515cc6359b613ec9e
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-22 17:09:38 +0300

    Add application constants

--- app/src/main/java/com/pro/powerworkouts/util/Constants.java
+++ app/src/main/java/com/pro/powerworkouts/util/Constants.java
@@ -1,7 +1,13 @@
 package com.pro.powerworkouts.util;
 
+import com.pro.powerworkouts.network.ExerciseDbApi;
+import com.pro.powerworkouts.network.ExerciseDbClient;
+
 public class Constants {
   public static final String BASE_URL = "https://exercisedb.p.rapidapi.com/exercises/";
   public static final String HOST_QUERY_PARAMETER = "X-RapidAPI-Host";
   public static final String KEY_QUERY_PARAMETER = "X-RapidAPI-Key";
+  public static final String EXTRA_WORKOUT_CATEGORY = "workout_category";
+  public static final ExerciseDbApi CLIENT = ExerciseDbClient.getClient();
+  public static final String EXTRA_WORKOUT_ID = "workout_id";
 }
