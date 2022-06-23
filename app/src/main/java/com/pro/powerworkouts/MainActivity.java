4342c4058a3d2923f335104078b3d95e21f35bd2
mansamusah254 <mansamusah830@gmail.com>
2022-06-23 10:00:18 +0300

    Signup activity

--- app/src/main/java/com/pro/powerworkouts/MainActivity.java
+++ app/src/main/java/com/pro/powerworkouts/MainActivity.java
@@ -2,13 +2,33 @@ package com.pro.powerworkouts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView powerWorkout;
+    private TextView sloganText;
+    private ImageView powerImage;
+    private Button getStartedButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getStartedButton = (Button) findViewById(R.id.getStartedButton);
+        getStartedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
