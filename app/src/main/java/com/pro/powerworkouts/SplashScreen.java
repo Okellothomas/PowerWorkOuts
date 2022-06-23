cc21cb4be3d085d68901efe654459010ea5ce858
mansamusah254 <mansamusah830@gmail.com>
2022-06-23 12:24:22 +0300

    splash screen

--- app/src/main/java/com/pro/powerworkouts/SplashScreen.java
+++ app/src/main/java/com/pro/powerworkouts/SplashScreen.java
@@ -5,27 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
+import com.pro.powerworkouts.ui.HomeActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
     LottieAnimationView lottie;
+    TextView powerWorkout;
+    TextView sloganText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        lottie = findViewById(R.id.lottie);
-        lottie.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
 
+        sloganText = findViewById(R.id.sloganText);
+        powerWorkout = findViewById(R.id.powerWorkout);
+        lottie = findViewById(R.id.lottie);
+
+
+        sloganText.animate().translationX(-1400).setDuration(2000).setStartDelay(3000);
+        powerWorkout.animate().translationX(-1400).setDuration(2000).setStartDelay(3000);
+        lottie.animate().translationX(2000).setDuration(2500).setStartDelay(3000);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                Intent i = new Intent(getApplicationContext(),HomeActivity.class);
                 startActivity(i);
             }
-        },5000);
+        },5500);
     }
 }
