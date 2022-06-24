c08cff234a05a85c7a737de26ad09841aec60d69
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-06-24 07:31:10 +0300

    Update logic

--- app/src/main/java/com/pro/powerworkouts/ui/SignUpActivity.java
+++ app/src/main/java/com/pro/powerworkouts/ui/SignUpActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -52,6 +53,7 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         mLoginText.setOnClickListener(this);
         mCreateUser.setOnClickListener(this);
 
+        FirebaseApp.initializeApp(this);
         mAuthenticate = FirebaseAuth
                 .getInstance();
 
